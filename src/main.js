f79a739939373caa866cf40999fd451743039038
Benoit Hautot <b.hautot@quantiq.nc>
2025-02-06 18:32:47 +1100

    Enhance styling with gradient background for body.
    Removed existing style rules and replaced them with a gradient background for the body. Also included the new custom stylesheet in the project to ensure it loads correctly.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).mount('#app')
